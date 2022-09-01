84bdca25fd75403fd4026ff6bdc88745d76ff712
Timothy <lemayiant4@gmail.com>
2022-09-01 19:13:26 +0300

    functional

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ document.getElementById("animal-image").addEventListener("click", () => {
       document.getElementById("the-details").textContent=`The ${data.name} latin\'s name is ${data.latin_name}. It is a ${data.animal_type} whose active time is ${data.active_time}. It measures between ${data.length_min}metres and ${data.length_max}metres in length. It weighs between ${data.weight_min}kiilograms and ${data.weight_max}kilograms. It has a lifespan of ${data.lifespan} years and spends most of its life on ${data.habitat}. They can be found in ${data.geo_range}`;
    });
 })
+document.addEventListener("DOMContentLoaded", () =>{
+    document.getElementById("th44").addEventListener("click", function () {
+        console.log("haroo")
+        alert("another html page!");
+    })
+    document.getElementById("butt2").addEventListener("click", () => alert("takes you to another html page"));
+    
+    document.getElementById("hhfg8").addEventListener("submit", (e) => {
+        e.preventDefault();
+        document.getElementById("hhfg8").reset()
+    })
+})
